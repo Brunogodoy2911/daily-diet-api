28eb1e7abad2fcf8923684a20f847f2ed2f2fadf
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-11-08 21:01:37 -0300

    :sparkles: Testes para operações CRUD

---
+++ models/meal.py
@@ -0,0 +1,18 @@
+from database import db
+
+class Meal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=True)
+    datetime = db.Column(db.DateTime, nullable=False)
+    is_diet = db.Column(db.Boolean, nullable=False, default=False)
+
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'datetime': self.datetime.isoformat(),
+            'is_diet': self.is_diet
+        }
